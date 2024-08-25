a26a614f14b31fe767ee06b1deb5f7af4cd120b0
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2024-08-25 11:46:07 -0300

    installed lazyVim

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,22 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then -- if the lazypath directory does not exist run this code
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/LazyVim/LazyVim.git",
+		"--branch=stable", -- latest stable release
+		lazypath
+	})
+	if vim.v.shell_error ~= 0 then -- error handling if cloning the repo goes wrong
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim\n", "ErrorMsg" },
+			{ out "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+
+vim.opt.rtp:prepend(lazypath) -- add lazy path to the runtime path
