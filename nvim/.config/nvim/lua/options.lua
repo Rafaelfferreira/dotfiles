9d1df212f52788b68459fe9aa61e7da36110f984
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2024-08-25 11:01:07 -0300

    added vim options about identation

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,5 +1,10 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = true -- vim.opt allow us to access vim options as a table. the properties names can be found using the command :options
+vim.opt.splitbelow = true -- makes new windows appear below the current window, not on top
+vim.opt.splitright = true
+
+vim.opt.tabstop = 4 -- Change the amount of spaces in a tab to 4
+vim.opt.shiftwidth = 4 -- Change the amount of spaces on a shift
 
 -- Keymapping
 vim.g.mapleader = " " -- the "g" stands for global
